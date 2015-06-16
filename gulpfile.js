33cd60251bd16abdc61f31a425f5e26300d0c4f8
ravila <sergionearbpo@hilbert.local>
2015-06-15 22:08:47 -0500

    mocha test runner gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,19 @@
 'use strict';
 
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
 gulp.task('default', function () {
 	// place code for default task here
 	console.log('gulp default task');
+});
+
+gulp.task('test', function () {
+	return gulp.src(['mocha-test-runner/*.spec.js'], { read: false })
+		.pipe(mocha({
+			reporter: 'spec',
+			globals: {
+				should: require('should')
+			}
+		}));
 });

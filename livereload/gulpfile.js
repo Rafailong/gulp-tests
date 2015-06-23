f39bbf93dbf747470b48767926173fef2f9b7456
ravila <sergionearbpo@hilbert.local>
2015-06-23 17:42:18 -0500

    static site livereload test

---
+++ livereload/gulpfile.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
+});
